4b9f42f70abd9bfdddc5680d19230f24b7209d46
Medrine-Jepkemoi <jepkemoimedrine@gmail.com>
2023-03-25 09:37:50 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -26,9 +26,24 @@ function moveDodgerLeft() {
     }
   }
 
+  function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+
+  }
+}
+
   document.addEventListener("keydown", function (e) {
     if (e.key === "ArrowLeft") {
       moveDodgerLeft();
     }
+    else {
+        moveDodgerRight();
+    }
   });
 
+
+//   if (e.key === "ArrowRight")
